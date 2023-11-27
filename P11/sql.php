40db8e0ed16701a3773f702c112b2dc8e06c81bd
Bagusoko <151505054+Bagusoko@users.noreply.github.com>
2023-11-27 15:36:41 +0800

    Add files via upload

---
+++ P11/sql.php
@@ -0,0 +1,10 @@
+<?php
+
+	$server		= "localhost"; 
+	$user		= "root";
+	$password	= ""; 
+	$database	= "2101020059_bagus oko kusuma"; 
+
+	$con = mysqli_connect($server,$user,$password,$database);
+
+?>
